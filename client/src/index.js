9c70f1e8d38cda4e5751fccf51652ecd93182b7f
DENIS PLASTER <denjeo@gmail.com>
2018-12-26 23:05:52 -0600

    profile info added

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "./index.css";
 
-function Greeting() {
-  return <h1>hello from React</h1>;
+function People() {
+  const friends = [
+    { name: "john", job: "developer", age: "23", company: "apple" },
+    { name: "bob", job: "designer", age: "22", company: "facebook" },
+    { name: "susy", job: "artist", age: "21", company: "twitter" }
+  ];
+
+  return (
+    <section>
+      <Person person={friends[0]}>
+        <p>some info about me</p>
+      </Person>
+      <Person person={friends[1]} />
+      <Person person={friends[2]} />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Person = props => {
+  const { name, job, age, company } = props.person;
+  const { children } = props;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      {children}
+      <p>{job}</p>
+      <p>{age}</p>
+      <p>{company}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<People />, document.getElementById("root"));
